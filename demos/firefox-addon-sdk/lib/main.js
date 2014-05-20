258aecaf433b2370aa7d3cac43661783ce491df3
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-05-20 14:48:52 -0700

    add popup to add-on sdk demo

--- demos/firefox-addon-sdk/lib/main.js
+++ demos/firefox-addon-sdk/lib/main.js
@@ -1,3 +1,33 @@
+var { ToggleButton } = require("sdk/ui/button/toggle");
+var panels = require("sdk/panel");
+var self = require("sdk/self");
+
+var button = ToggleButton({
+  id: "my-button",
+  label: "my button",
+  icon: {
+    "32": "./icon.png"
+  },
+  onChange: handleChange
+});
+
+var panel = panels.Panel({
+  contentURL: self.data.url("popup.html")
+});
+
+function handleChange(state) {
+  if (state.checked) {
+    panel.show({
+      width: 375,
+      height: 465,
+      position: {
+        top: 0,
+        right: 0
+      }
+    });
+  }
+}
+
 var buttons = require("sdk/ui/button/action");
 var tabs = require("sdk/tabs");
 
@@ -7,10 +37,12 @@ var button = buttons.ActionButton({
   icon: {
     "32": "./icon.png",
   },
-  onClick: function handleClick(state) {
-    tabs.open("https://www.google.com/search?q=cat&tbm=isch");
-  }
+  onClick: handleClick
 });
+
+function handleClick(state) {
+  tabs.open("https://www.google.com/search?q=cat&tbm=isch");
+}
 
 var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
